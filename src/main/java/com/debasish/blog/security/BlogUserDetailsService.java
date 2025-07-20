c5ea24811899081789f86069265c6c34ad2366a4
debasish1708 <debasishdas1708@gmail.com>
2025-07-20 18:34:19 +0530

    setup BlogUser

---
+++ src/main/java/com/debasish/blog/security/BlogUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.debasish.blog.security;
+
+import com.debasish.blog.domain.entities.User;
+import com.debasish.blog.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class BlogUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found " + email));
+        return new BlogUserDetails(user);
+    }
+}
